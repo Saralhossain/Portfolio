376ca51f14e65b3ee376f00722071070d83e6815
Saralhossain <alamdar.hussain0007@gmail.com>
2022-07-10 06:46:26 +0500

    Work Experience

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Intro from "./Components/Intro/intro";
 import './App.css';
 import Services from "./Components/Services/Services";
 import Experience from "./Components/Experience/Experience";
+import Work from "./Components/Work/Work";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Intro/>
       <Services/>
       <Experience/>
+      <Work/>
     </div>
   );
 }
---
+++ src/Components/Work/Work.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import "./Work.css";
+import Upwork from '../../img/Upwork.png';
+import Fiverr from '../../img/fiverr.png';
+// import Amazon from '../../img/amazon.png';
+import Shopify from '../../img/Shopify.png';
+import Facebook from '../../img/Facebook.png';
+import Malook from '../../img/malook.png';
+
+
+const Work = () =>{
+
+    return ( 
+        <div className='works'>
+        {/* Left side */}
+            <div className="awesome">
+                <span>Work For All These</span>
+                <span>Brand & Clients</span>
+                <span>As a Software developer I am providing following services for my customers
+                <br/>
+                As a Software developer I am providing following
+                <br/>
+                As a Software developer I am providing following
+                <br/>
+                </span>
+                <button className="button s-button">Hire Me</button>
+                <div className="blur s-blur1" style={{background:'#ABF1FF94'}}>
+                </div>
+            </div>
+            {/* Right side */}
+            <div className='w-right'>
+                <div className='w-mainCircle'>
+                    <div className='w-secCircle'>
+                        <img src={Upwork} alt=""/>
+                    </div>
+                    <div className='w-secCircle'>
+                        <img src={Fiverr} alt=""/>
+                    </div>
+                    <div className='w-secCircle'>
+                        <img src={Malook} style={{transform:'scale(0.3)'}} alt=""/>
+                    </div>
+                    <div className='w-secCircle'>
+                        <img src={Shopify} alt=""/>
+                    </div>
+                    <div className='w-secCircle'>
+                        <img src={Facebook} alt=""/>
+                    </div>
+                </div>
+
+                <div className='w-backCircle blueCircle'></div>
+                <div className='w-backCircle yellowCircle'></div>
+                <div className='w-backCircle greenCircle'></div>
+                <div className='w-backCircle blackCircle'></div>
+            
+            </div>
+        </div>
+    )
+}
+
+
+export default Work;
