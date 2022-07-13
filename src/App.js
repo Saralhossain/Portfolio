0a19caf63a85f48c491a78459514b7454f7022c6
Saralhossain <alamdar.hussain0007@gmail.com>
2022-07-13 05:28:43 +0500

    toggling dark/bright mode effect

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import Portfolio from "./Components/Portfolio/Portfolio";
 import Testimonial from "./Components/Testimonial/Testimonial";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
-
+import { themeContext } from "./Context";
+import { useContext } from "react";
 
 function App() {
+    const theme = useContext(themeContext);
+    const darkMode = theme.state.darkMode;
+
+
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      background: darkMode? 'black' : '',
+      color:darkMode? 'white' : ''
+    }}
+    >
       <Navbar/>
       <Intro/>
       <Services/>
